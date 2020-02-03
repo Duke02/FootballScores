25ab4bfaea10cdbba28860e37513fe5bdc353754
Trystan May <trystanmay@gmail.com>
2020-02-02 18:50:52 -0600

    Added field goal and touchdown calculation

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import numpy as np
 
 def main():
-    print("Hello World!")
+    MAX_LENGTH: int = 20
+    numbers: np.ndarray = np.zeros((MAX_LENGTH, MAX_LENGTH), dtype=int)
 
+    for field_goal in range(1, MAX_LENGTH):
+        numbers[field_goal, 0] = numbers[field_goal - 1, 0] + 3
+
+    for touchdown in range(1, MAX_LENGTH):
+        numbers[0, touchdown] = numbers[0, touchdown - 1] + 6
+
+    for touchdown in range(1, MAX_LENGTH):
+        prev_touchdown: int = touchdown - 1
+        for field_goal in range(1, MAX_LENGTH):
+            numbers[field_goal, touchdown] = numbers[field_goal, prev_touchdown] + 6
+
+    print(numbers)
 
 if __name__ == "__main__":
     main()
